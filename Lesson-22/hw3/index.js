cef0a79105e6e5663e1c9594d66851f4e18df6d6
Shulha Nikita <shulgamalthael@gmail.com>
2020-08-18 14:07:54 +0300

    mod

--- Lesson-22/hw3/index.js
+++ Lesson-22/hw3/index.js
@@ -1,5 +1,5 @@
 const checkboxElem = document.querySelector('.task-status');
-checkboxElem.addEventListener('click', () => {
+checkboxElem.addEventListener('change', () => {
     // console.log(checkboxElem.value);
     checkboxElem.checked ? console.log(true) : console.log(false);
 });
