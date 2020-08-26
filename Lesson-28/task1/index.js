33ac4fff157753a3053adfd88412cd7fb88126c1
Shulha Nikita <shulgamalthael@gmail.com>
2020-08-26 17:19:21 +0300

    mod

--- Lesson-28/task1/index.js
+++ Lesson-28/task1/index.js
@@ -1,39 +1,31 @@
-const calc = initlValue => {
-
-    let result = initlValue;
+export const calc = initValue => {
+    let result = initValue;
 
     const calculator = {
         add(value) {
             result += value;
+            return calculator;
+        },
+        subtract(value) {
+            result -= value;
             return this;
         },
-
         mult(value) {
             result *= value;
             return this;
         },
-
-        substract(value) {
-            result -= value;
-            return this;
-        },
-
         div(value) {
             result /= value;
             return this;
         },
-
         result() {
-            return result
+            return result;
         }
     };
 
     return calculator;
-
 };
 
-const result = calc(3).add(2).mult(4).div(10).substract(5).result(); // -3
+// const result = calc(3).add(2).mult(4).div(10).subtract(5).result();
 
 // console.log(result);
-
-export { calc };
