75c004da776cd6b3b40ab84eae9f1f7bdd5a6866
Shulha Nikita <shulgamalthael@gmail.com>
2020-08-26 17:13:43 +0300

    init files

---
+++ Lesson-28/task1/index.js
@@ -0,0 +1,38 @@
+
+const calc = initialValue => {
+
+    let result = initialValue;
+
+    const calculator = {
+        add(value) {
+            result += value;
+            return this;
+        },
+
+        mult(value) {
+            result *= value;
+            return this;
+        },
+
+        substract(value) {
+            result -= value;
+            return this;
+        },
+
+        div(value) {
+            result /= value;
+            return this;
+        },
+
+        result() {
+            return result
+        }
+    };
+
+    return calculator;
+
+};
+
+const result = calc(3).add(2).mult(4).div(10).substract(5).result(); // -3
+
+console.log(result);
