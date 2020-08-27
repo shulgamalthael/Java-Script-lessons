5924b59d3da08e7e9a8df8adbb146f1354e3d61e
Shulha Nikita <shulgamalthael@gmail.com>
2020-08-27 12:50:30 +0300

    mod

--- Lesson-28/task4/index.js
+++ Lesson-28/task4/index.js
@@ -1,15 +1,18 @@
-const add3 = value => value + 3;
-const mult2 = value => value * 2;
-const div4 = value => value / 4;
+const addNum = value => value + 3;
+const multNum = value => value * 2;
+const divNum = value => value / 4;
+
 
 const compose = (...funcs) => value => {
-    return funcs.reduce((acc, func) => func(acc), value); 
+    return funcs.reduce((acc, func) => func(acc), value);
 };
 
 const doEverything = compose(
-    add3,
-    mult2,
-    div4,
+    addNum,
+    multNum,
+    divNum,
 );
 
-console.log(doEverything(3));
+console.log(doEverything(9));
+
+export { compose };
