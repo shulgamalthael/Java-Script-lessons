ce47d34922aee5c15a674e94fd33b392d15f1d68
Shulha Nikita <shulgamalthael@gmail.com>
2020-08-29 13:02:06 +0300

    mod

--- Lesson-29/task1/index.js
+++ Lesson-29/task1/index.js
@@ -1,6 +1,9 @@
+//add image on the page and show it sizes.
+
 const addImage = (imgSrc, callback) => {
+
     const imgElem = document.createElement('img');
-    imgElem.setAttribute('alt', 'My Photo');
+    imgElem.setAttribute('Alt', 'My Photo');
     imgElem.src = imgSrc;
 
     const containerElem = document.querySelector('.page');
@@ -8,28 +11,32 @@ const addImage = (imgSrc, callback) => {
 
     const onImageLoaded = () => {
         const { width, height } = imgElem;
-        // console.log(imgElem);
-
         callback(null, { width, height });
-    };
+    }
 
+    //image is load
     imgElem.addEventListener('load', onImageLoaded);
 
-    imgElem.addEventListener('error', () => callback('Image load failed'))
+    imgElem.addEventListener('error', () => callback('Image Load Failed'));
+    //imgElem.width();
+    //imgElem.height();
+    // const { width, height } = imgElem;
+
 };
 
-// const imgSrc = 'https://server.com/image.png';
+const imgSrc = "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg";
 
 const onImageLoaded = (error, data) => {
-    if (error) {
+    
+    if(error) {
         console.log(error);
         return;
     }
+
     const { width, height } = data;
     const sizeElem = document.querySelector('.image-size');
-    sizeElem.textContent = `${width}: x ${height}`;
-};
-
-// addImage(imgSrc, onImageLoaded);
+    sizeElem.textContent = `${width} x ${height}`;
+}
+addImage(imgSrc, onImageLoaded);
 
 export { addImage };
