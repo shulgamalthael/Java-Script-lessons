e528077ef7fd8287671b357ac522610002ab2a9d
Shulha Nikita <shulgamalthael@gmail.com>
2020-08-30 15:22:02 +0300

    init files

---
+++ Lesson-30/task1/index.js
@@ -0,0 +1,30 @@
+//add image
+
+const addImage = imgSrc => {
+    const p = new Promise ((resolveCb , rejectCb) => {
+        const imgElem = document.createElement('img');
+        imgElem.setAttribute('alt', 'My photo');
+        imgElem.src = imgSrc;
+        const containerElem = document.querySelector('.page')
+        containerElem.append(imgElem);
+      
+        const onImageLoaded = () => {
+          const { width, height } = imgElem;
+          resolveCb({ width, height });
+        };
+        
+        imgElem.addEventListener('load', onImageLoaded);
+      
+        imgElem.addEventListener('error', () =>rejectCb(new Error('Image load failed')));
+      
+    });
+
+    return p;
+}
+
+const imgSrc = 'https://server.com/image.png';
+const resultPromise = addImageWithPromise(imgSrc);
+
+resultPromise.catch(error => console.log(error));
+
+export { addImage };
