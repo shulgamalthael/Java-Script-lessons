4d6996055863edc76f05c69a7eaddc886466bf25
Shulha Nikita <shulgamalthael@gmail.com>
2020-08-30 15:22:51 +0300

    mod

--- Lesson-30/task1/index.js
+++ Lesson-30/task1/index.js
@@ -1,30 +1,29 @@
-//add image
-
 const addImage = imgSrc => {
-    const p = new Promise ((resolveCb , rejectCb) => {
+    const p = new Promise((resolveCb, rejectCb) => {
         const imgElem = document.createElement('img');
-        imgElem.setAttribute('alt', 'My photo');
+        imgElem.setAttribute('alt', 'My Photo');
         imgElem.src = imgSrc;
-        const containerElem = document.querySelector('.page')
-        containerElem.append(imgElem);
-      
-        const onImageLoaded = () => {
-          const { width, height } = imgElem;
-          resolveCb({ width, height });
-        };
-        
-        imgElem.addEventListener('load', onImageLoaded);
-      
-        imgElem.addEventListener('error', () =>rejectCb(new Error('Image load failed')));
-      
-    });
 
+        const containerElem = document.querySelector('.page');
+        containerElem.append(imgElem);
+
+        const onImageLoaded = () => {
+            const { width, height } = imgElem;
+            resolveCb({ width, height });
+        };
+
+        imgElem.addEventListener('load', onImageLoaded);
+
+        imgElem.addEventListener('error', () => rejectCb(new Error('Image load failed')))
+    });
     return p;
-}
+};
 
 const imgSrc = 'https://server.com/image.png';
-const resultPromise = addImageWithPromise(imgSrc);
+
+const resultPromise = addImage(imgSrc);
 
 resultPromise.catch(error => console.log(error));
+
 
 export { addImage };
