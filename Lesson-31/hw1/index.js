f4aef0644a031a190c122d5ea9a1074d63bc9adf
Shulha Nikita <shulgamalthael@gmail.com>
2020-08-31 07:20:32 +0300

    init files

---
+++ Lesson-31/hw1/index.js
@@ -0,0 +1,37 @@
+const successPromise = new Promise(resolve => {
+    resolve(32);
+});
+
+/*
+ * исправьте цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+ */
+
+successPromise
+    .then(number => {
+        const halfNumber = number / 2;
+        return halfNumber;
+    })
+    .then(number => {
+        /* fix this handler */
+        const squaredNumber = number * number;
+        return squaredNumber;
+    })
+    .then(result => {
+        console.log(result); // 256
+    });
+
+/*
+ * исправьте цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+ */
+successPromise
+    .then(number => {
+        /* fix this handler */
+        // console.log(number)
+        const num = number + 288;
+        return num;
+    })
+    .then(result => {
+        console.log(result); // 320
+    });
+
+console.log('!!! Обратите внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!');
