4b76d2c055388764e326d6d905bb687fa39b0a21
Shulha Nikita <shulgamalthael@gmail.com>
2020-08-31 14:29:20 +0300

    mod

--- Lesson32/task1/index.js
+++ Lesson32/task1/index.js
@@ -15,7 +15,7 @@ const getSum = numbers => numbers
     .filter(value => !isNaN(value))   
     .reduce((acc, num) => acc + Number(num), 0);
 
-const asyncSum = (...asyncNumbers) => {
+export const asyncSum = (...asyncNumbers) => {
     return Promise.all(asyncNumbers)
         .then(numbers => getSum(numbers))
         .catch(() => Promise.reject(new Error('Can\`t calculate')));
