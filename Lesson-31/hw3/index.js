4b8daacba435a032702f7a025b93952cc9cff5fe
Shulha Nikita <shulgamalthael@gmail.com>
2020-08-31 07:24:08 +0300

    mod

---
+++ Lesson-31/hw3/index.js
@@ -0,0 +1,12 @@
+const delay = ms => {
+    return new Promise(resolve => {
+        setTimeout(() =>
+            resolve(), ms)
+    })
+};
+
+delay(3000).then(() => {
+    console.log('Done');
+});
+
+export { delay };
