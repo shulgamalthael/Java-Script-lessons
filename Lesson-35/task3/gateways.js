c5622f938d3f0b1f4127dad5b7c6e35df211b5c7
Shulha Nikita <shulgamalthael@gmail.com>
2020-09-06 16:42:53 +0300

    init files

---
+++ Lesson-35/task3/gateways.js
@@ -0,0 +1,18 @@
+export const fetchUserData = userName => {
+    return fetch(`https://api.github.com/users/${userName}`)
+        .then(response => {
+            if ( response.status === 200 ) {
+                return response.json();
+            }
+            throw new Error('Falied to load data');
+        });
+};
+
+export const fetchRepositories = url =>
+    fetch(url)
+        .then(response => {
+            if ( response.ok ) {
+                return response.json();
+            }
+            throw new Error('Falied to load data');
+        });
